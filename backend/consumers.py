4f72891f8b6b0a7ab374d1d8ad19538bbe5436b5
Abdo-ox <abderrahim.jari6@gmail.com>
2025-07-26 23:18:48 +0100

    hello

--- backend/consumers.py
+++ backend/consumers.py
@@ -5,6 +5,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.channel_layer.group_add("notifications", self.channel_name)
         await self.accept()
+        await self.send(text_data=json.dumps({"hello": "hello"}))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("notifications", self.channel_name)
