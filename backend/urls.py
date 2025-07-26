782217001149739ef706803b56ee46e56a2e0267
Abdo-ox <abderrahim.jari6@gmail.com>
2025-07-26 23:00:48 +0100

    hello

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 def home(request):
